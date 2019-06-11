9a88bc45f83af68ac1702c952fe2bb26276fa4ec
andrei hosu <h.andrei92@gmail.com>
2019-06-11 12:31:38 +0300

    Updates

---
+++ precache-manifest.4aeb330d29669ecdcb7eda34c5207580.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "238af20f512412d0c9299e98254e34cd",
+    "url": "/portfolio/index.html"
+  },
+  {
+    "revision": "acd5a31e5ee7cc0e0b7c",
+    "url": "/portfolio/static/css/main.c7b9b96a.chunk.css"
+  },
+  {
+    "revision": "a66621c6d1343b30f817",
+    "url": "/portfolio/static/js/2.2a6eb809.chunk.js"
+  },
+  {
+    "revision": "acd5a31e5ee7cc0e0b7c",
+    "url": "/portfolio/static/js/main.58816578.chunk.js"
+  },
+  {
+    "revision": "0fad4c0f10d57b530fe4",
+    "url": "/portfolio/static/js/runtime~main.784d01ab.js"
+  }
+]);
