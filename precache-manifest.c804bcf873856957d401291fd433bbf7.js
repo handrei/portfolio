263fe10f861249771d462b87e4229ddc79863ae4
andrei hosu <h.andrei92@gmail.com>
2019-06-11 13:11:08 +0300

    Updates

--- precache-manifest.c804bcf873856957d401291fd433bbf7.js
+++ precache-manifest.c804bcf873856957d401291fd433bbf7.js
@@ -1,10 +1,10 @@
 self.__precacheManifest = (self.__precacheManifest || []).concat([
   {
-    "revision": "238af20f512412d0c9299e98254e34cd",
+    "revision": "e4f9d0323274e849c0c3c2b37ef80f2c",
     "url": "/portfolio/index.html"
   },
   {
-    "revision": "acd5a31e5ee7cc0e0b7c",
+    "revision": "31fe2dfbe0a024b33d30",
     "url": "/portfolio/static/css/main.c7b9b96a.chunk.css"
   },
   {
@@ -12,8 +12,8 @@ self.__precacheManifest = (self.__precacheManifest || []).concat([
     "url": "/portfolio/static/js/2.2a6eb809.chunk.js"
   },
   {
-    "revision": "acd5a31e5ee7cc0e0b7c",
-    "url": "/portfolio/static/js/main.58816578.chunk.js"
+    "revision": "31fe2dfbe0a024b33d30",
+    "url": "/portfolio/static/js/main.8871cd07.chunk.js"
   },
   {
     "revision": "0fad4c0f10d57b530fe4",
