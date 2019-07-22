6cfced76be475f73170a6ba2e1c59c4cae329599
andrei hosu <h.andrei92@gmail.com>
2019-07-22 17:18:50 +0300

    embed responsive videos

--- src/pages/Portfolio.js
+++ src/pages/Portfolio.js
@@ -9,57 +9,69 @@ export default class Portfolio extends Component {
       >
         <div className='w-100'>
           <h2 className='mb-5'>Portfolio</h2>
-          <iframe
-            title='devconnector'
-            width='560'
-            height='315'
-            src='https://www.youtube.com/embed/cna1pUCoYGY'
-            frameborder='0'
-            allow='accelerometer; autoplay; encrypted-media; gyroscope; picture-in-picture'
-            allowfullscreen
-          />
+          <div className='embed-responsive embed-responsive-16by9'>
+            <iframe
+              className='embed-responsive-item'
+              title='devconnector'
+              width='560'
+              height='315'
+              src='https://www.youtube.com/embed/cna1pUCoYGY'
+              frameborder='0'
+              allow='accelerometer; autoplay; encrypted-media; gyroscope; picture-in-picture'
+              allowfullscreen
+            />
+          </div>
           <br />
           <span>
             made with: <strong>react, redux, nodejs, express, mongodb </strong>
           </span>
           <hr />
-          <iframe
-            title='shopping app'
-            width='560'
-            height='315'
-            src='https://www.youtube.com/embed/5GmKOphrhBk'
-            frameborder='0'
-            allow='accelerometer; autoplay; encrypted-media; gyroscope; picture-in-picture'
-            allowfullscreen
-          />
+          <div className='embed-responsive embed-responsive-16by9'>
+            <iframe
+              className='embed-responsive-item'
+              title='shopping app'
+              width='560'
+              height='315'
+              src='https://www.youtube.com/embed/5GmKOphrhBk'
+              frameborder='0'
+              allow='accelerometer; autoplay; encrypted-media; gyroscope; picture-in-picture'
+              allowfullscreen
+            />
+          </div>
           <br />
           <span>
             made with: <strong> react, nodejs, express, mongodb </strong>
           </span>
           <hr />
-          <iframe
-            title='tracalorie'
-            width='560'
-            height='315'
-            src='https://www.youtube.com/embed/1wGyg2CKS5E'
-            frameborder='0'
-            allow='accelerometer; autoplay; encrypted-media; gyroscope; picture-in-picture'
-            allowfullscreen
-          />
+          <div className='embed-responsive embed-responsive-16by9'>
+            <iframe
+              className='embed-responsive-item'
+              title='tracalorie'
+              width='560'
+              height='315'
+              src='https://www.youtube.com/embed/1wGyg2CKS5E'
+              frameborder='0'
+              allow='accelerometer; autoplay; encrypted-media; gyroscope; picture-in-picture'
+              allowfullscreen
+            />
+          </div>
           <br />
           <span>
             made with: <strong>vanilla js</strong>
           </span>
           <hr />
-          <iframe
-            title='social-ape'
-            width='560'
-            height='315'
-            src='https://www.youtube.com/embed/jNk72jFiB8g'
-            frameborder='0'
-            allow='accelerometer; autoplay; encrypted-media; gyroscope; picture-in-picture'
-            allowfullscreen
-          />
+          <div className='embed-responsive embed-responsive-16by9'>
+            <iframe
+              className='embed-responsive-item'
+              title='social-ape'
+              width='560'
+              height='315'
+              src='https://www.youtube.com/embed/jNk72jFiB8g'
+              frameborder='0'
+              allow='accelerometer; autoplay; encrypted-media; gyroscope; picture-in-picture'
+              allowfullscreen
+            />
+          </div>
           <br />
           <span>
             made with:
