ec8363055814bba32f3aa839996f042ff28d0dbd
Tudor Pop <tudor.dan.pop7@gmail.com>
2019-12-07 17:46:30 +0200

    Updates

--- precache-manifest.6d6a1f4546052fd633da8a78caef786c.js
+++ precache-manifest.6d6a1f4546052fd633da8a78caef786c.js
@@ -1,10 +1,10 @@
 self.__precacheManifest = (self.__precacheManifest || []).concat([
   {
-    "revision": "f7fcd7f4d78a33a54e6687224a0e45a3",
+    "revision": "6fc58aff301e8dfcbb318856a797fa20",
     "url": "/portfolio/index.html"
   },
   {
-    "revision": "ad67621b8a206c87a1f2",
+    "revision": "24eefef433d7fb4d6452",
     "url": "/portfolio/static/css/main.c7b9b96a.chunk.css"
   },
   {
@@ -12,8 +12,8 @@ self.__precacheManifest = (self.__precacheManifest || []).concat([
     "url": "/portfolio/static/js/2.2a6eb809.chunk.js"
   },
   {
-    "revision": "ad67621b8a206c87a1f2",
-    "url": "/portfolio/static/js/main.1e28b0b8.chunk.js"
+    "revision": "24eefef433d7fb4d6452",
+    "url": "/portfolio/static/js/main.2dbbf430.chunk.js"
   },
   {
     "revision": "0fad4c0f10d57b530fe4",
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/portfolio/precache-manifest.c30bfa3874ce43015887f10f0e101ea1.js"
+  "/portfolio/precache-manifest.6d6a1f4546052fd633da8a78caef786c.js"
 );
 
 self.addEventListener('message', (event) => {
