8695e425945a2865d162ff00b56397b59629984c
Tudor Pop <tudor.dan.pop7@gmail.com>
2019-12-07 17:43:35 +0200

    added geoPins app

--- src/pages/Portfolio.js
+++ src/pages/Portfolio.js
@@ -1,23 +1,23 @@
-import React, { Component } from 'react';
+import React, { Component } from "react";
 
 export default class Portfolio extends Component {
   render() {
     return (
       <section
-        className='resume-section p-3 p-lg-5 d-flex align-items-center'
-        id='interests'
+        className="resume-section p-3 p-lg-5 d-flex align-items-center"
+        id="interests"
       >
-        <div className='w-100'>
-          <h2 className='mb-5'>Portfolio</h2>
-          <div className='embed-responsive embed-responsive-16by9'>
+        <div className="w-100">
+          <h2 className="mb-5">Portfolio</h2>
+          <div className="embed-responsive embed-responsive-16by9">
             <iframe
-              className='embed-responsive-item'
-              title='devconnector'
-              width='560'
-              height='315'
-              src='https://www.youtube.com/embed/cna1pUCoYGY'
-              frameborder='0'
-              allow='accelerometer; autoplay; encrypted-media; gyroscope; picture-in-picture'
+              className="embed-responsive-item"
+              title="devconnector"
+              width="560"
+              height="315"
+              src="https://www.youtube.com/embed/cna1pUCoYGY"
+              frameborder="0"
+              allow="accelerometer; autoplay; encrypted-media; gyroscope; picture-in-picture"
               allowfullscreen
             />
           </div>
@@ -26,15 +26,15 @@ export default class Portfolio extends Component {
             made with: <strong>react, redux, nodejs, express, mongodb </strong>
           </span>
           <hr />
-          <div className='embed-responsive embed-responsive-16by9'>
+          <div className="embed-responsive embed-responsive-16by9">
             <iframe
-              className='embed-responsive-item'
-              title='shopping app'
-              width='560'
-              height='315'
-              src='https://www.youtube.com/embed/5GmKOphrhBk'
-              frameborder='0'
-              allow='accelerometer; autoplay; encrypted-media; gyroscope; picture-in-picture'
+              className="embed-responsive-item"
+              title="shopping app"
+              width="560"
+              height="315"
+              src="https://www.youtube.com/embed/5GmKOphrhBk"
+              frameborder="0"
+              allow="accelerometer; autoplay; encrypted-media; gyroscope; picture-in-picture"
               allowfullscreen
             />
           </div>
@@ -43,15 +43,15 @@ export default class Portfolio extends Component {
             made with: <strong> react, nodejs, express, mongodb </strong>
           </span>
           <hr />
-          <div className='embed-responsive embed-responsive-16by9'>
+          <div className="embed-responsive embed-responsive-16by9">
             <iframe
-              className='embed-responsive-item'
-              title='tracalorie'
-              width='560'
-              height='315'
-              src='https://www.youtube.com/embed/1wGyg2CKS5E'
-              frameborder='0'
-              allow='accelerometer; autoplay; encrypted-media; gyroscope; picture-in-picture'
+              className="embed-responsive-item"
+              title="tracalorie"
+              width="560"
+              height="315"
+              src="https://www.youtube.com/embed/1wGyg2CKS5E"
+              frameborder="0"
+              allow="accelerometer; autoplay; encrypted-media; gyroscope; picture-in-picture"
               allowfullscreen
             />
           </div>
@@ -60,15 +60,15 @@ export default class Portfolio extends Component {
             made with: <strong>vanilla js</strong>
           </span>
           <hr />
-          <div className='embed-responsive embed-responsive-16by9'>
+          <div className="embed-responsive embed-responsive-16by9">
             <iframe
-              className='embed-responsive-item'
-              title='social-ape'
-              width='560'
-              height='315'
-              src='https://www.youtube.com/embed/jNk72jFiB8g'
-              frameborder='0'
-              allow='accelerometer; autoplay; encrypted-media; gyroscope; picture-in-picture'
+              className="embed-responsive-item"
+              title="social-ape"
+              width="560"
+              height="315"
+              src="https://www.youtube.com/embed/jNk72jFiB8g"
+              frameborder="0"
+              allow="accelerometer; autoplay; encrypted-media; gyroscope; picture-in-picture"
               allowfullscreen
             />
           </div>
@@ -76,6 +76,23 @@ export default class Portfolio extends Component {
           <span>
             made with:
             <strong> react, redux, firebase, express, material-ui</strong>
+          </span>
+          <hr />
+          <div className="embed-responsive embed-responsive-16by9">
+            <iframe
+              width="560"
+              title="geo-pins"
+              height="315"
+              src="https://www.youtube.com/embed/FNzVksULqEM"
+              frameborder="0"
+              allow="accelerometer; autoplay; encrypted-media; gyroscope; picture-in-picture"
+              allowfullscreen
+            />
+          </div>
+          <br />
+          <span>
+            made with:
+            <strong> react hooks, context, graphQL, material-ui</strong>
           </span>
         </div>
       </section>
