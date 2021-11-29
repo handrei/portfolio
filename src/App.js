5602b26e621035a255cf17df4f49736f206dea7a
handrei <h.andrei92@gmail.com>
2021-11-29 08:33:35 +0200

    v2 initial commit

--- src/App.js
+++ src/App.js
@@ -1,6 +1,7 @@
 import React from 'react';
 import { BrowserRouter as Router, Route } from 'react-router-dom';
 import './App.css';
+
 import Navbar from './components/Navbar';
 import About from './pages/About';
 import Experience from './pages/Experience';
